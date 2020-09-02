4f24b839e9007454457d09c4246fb9401d5246bf
Carl Brunson <carl.brunson@gmail.com>
2020-09-01 21:43:33 -0500

    master

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Accordian from './components/Accordion';
+import Search from './components/Search';
 
 const items = [
   {
@@ -17,5 +18,8 @@ const items = [
 ];
 
 export default () => {
-  return <Accordian items={items} />;
+  return (
+  //  <Accordian items={items} />
+      <Search />
+  );
 };
